c338a3eb18ebf982214e7e1724e480518404011e
YuanZhou3118 <ZY318205253872>
2016-08-19 14:59:26 +0800

    upload config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhou
+ * Date: 16/8/19
+ * Time: 下午2:36
+ */
+define('TOKEN', 'your_token');
+$appID = 'your_appid';
+$appSecret = 'your_appsecret';
+$mysqlserver = 'localhost';
+$sqluser = 'root';
+$sqlpass = 'password';
+$sqldatabase = 'your_database_name';
